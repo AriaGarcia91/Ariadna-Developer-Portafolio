e17504147bdc95902ae9d4162c99774ec7c27ac8
AriaGarcia91 <karunska_91@hotmail.com>
2026-05-27 15:37:13 -0600

    Add files via upload

---
+++ Asp.Net/ReporteAuditoria/CPM.Reporte Auditoria/CPM.ReporteAuditoria/Models/ViewReporteAuditoriaModel.cs
@@ -0,0 +1,15 @@
+﻿using CPM.ReporteAuditoria.BusinessType;
+using System.Collections.Generic;
+
+
+namespace CPM.ReporteAuditoria.Models
+{
+	public class ViewReporteAuditoriaModel
+	{
+        public List<Sucursal> Oficinas { get; set; }
+        public List<Usuario> Usuarios { get; set; }
+        public Dictionary<int, string> TipoOficinas{ get; set; }
+        public Dictionary<int,string> Operaciones { get; set; }
+        public Dictionary<int,string> Eventos { get; set; }
+    }
+}
