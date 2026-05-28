a1df5edc2b943e9baeccf6c3c3aff804f142aab7
AriaGarcia91 <karunska_91@hotmail.com>
2026-05-27 18:57:26 -0600

    Add files via upload

---
+++ Asp.NetCore/CPM.Dummy.Api/CPM.Dummy.Api.BussinesInterface/IRespuestaProcessor.cs
@@ -0,0 +1,8 @@
+﻿namespace CPM.Dummy.BussinesInterface
+{
+    public interface IRespuestaProcessor
+    {
+        string ObtenerMensajeJson(string api, string firma);
+        string ObtenerDocumentoPdf(string api, string firma);
+    }
+}
