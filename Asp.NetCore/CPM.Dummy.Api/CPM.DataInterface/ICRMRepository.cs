211b253a7a7d36aa1228bc1b4d4fc7588696eb4f
AriaGarcia91 <karunska_91@hotmail.com>
2026-05-27 18:39:41 -0600

    Add files via upload

---
+++ Asp.NetCore/CPM.Dummy.Api/CPM.DataInterface/ICRMRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace CPM.Dummy.DataInterface
+{
+    public interface ICRMRepository
+    {
+        string ConsultaRespuestaApi(string api, string firma);
+        string ConsultaFetchRespuestaApi(string api, string firma);
+        string ConsultaFetchRespuestaApiDocumento(string api, string firma);
+        string ConsultaRespuestaApiDocumento(string api, string firma);
+    }
+}
