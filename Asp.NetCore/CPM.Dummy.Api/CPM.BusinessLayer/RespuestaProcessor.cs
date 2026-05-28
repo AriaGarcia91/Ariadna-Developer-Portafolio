577c5eba72da5e7794f7d386ca5fd2af8d15745f
AriaGarcia91 <karunska_91@hotmail.com>
2026-05-27 18:44:26 -0600

    Add files via upload

---
+++ Asp.NetCore/CPM.Dummy.Api/CPM.BusinessLayer/RespuestaProcessor.cs
@@ -0,0 +1,33 @@
+﻿using CPM.Dummy.BussinesInterface;
+using CPM.Dummy.DataInterface;
+using CPM.Dummy.OperationalManager;
+
+
+namespace CPM.Dummy.BussinesLayer
+{
+    public class RespuestaProcessor : IRespuestaProcessor
+    {
+        private readonly ICRMRepository _repository;
+        private readonly ILogger _logger;
+
+
+        public RespuestaProcessor(ICRMRepository repository, ILogger logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public string ObtenerMensajeJson(string api, string firma)
+        {
+            var mensajeJson = _repository.ConsultaRespuestaApi(api, firma);
+
+            return mensajeJson;
+        }
+
+        public string ObtenerDocumentoPdf(string api, string firma)
+        {
+            var documentoPdf64 = _repository.ConsultaRespuestaApiDocumento(api,firma);
+            return documentoPdf64;
+        } 
+    }
+}
