f462db9ac3a3a3cbddded0b7c07ab62632eb1bd4
AriaGarcia91 <karunska_91@hotmail.com>
2026-05-27 18:41:03 -0600

    Add files via upload

---
+++ Asp.NetCore/CPM.Dummy.Api/CPM.Dummy.BusinessType/RespuestaAPI.cs
@@ -0,0 +1,10 @@
+﻿namespace CPM.Dummy.BussinesType
+{
+    public class RespuestaAPI
+    {
+        public string Firma { get; set; }
+        public int MetodoHttp { get; set; }
+        public Api Api { get; set; }
+        public string MensajeJSON { get; set; }
+    }
+}
